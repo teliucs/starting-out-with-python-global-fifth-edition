448e7a33e239f5fd2800e1fc01498c3e0e3a16f1
teliucs <lorenzo.tegliucci@studenti.unipg.it>
2022-12-13 11:17:24 +0100

    Finished L6\14

--- L6_list_tuple/14.py
+++ L6_list_tuple/14.py
@@ -12,6 +12,7 @@
 import matplotlib.pyplot as plt
 
 def main():
+    print("--------------------------------------------")
     print("Welcome to your personal expenses asssistant")
     print("--------------------------------------------")
     print()
@@ -23,14 +24,19 @@ def main():
         if choice == 2:
             modify_data()
         else:
+            print('\n--------------------------------------------')
+            print("Thanks for your visit.")
+            print("--------------------------------------------\n")
             return
-
+        
 
 def get_choice():
+    """Get user choice"""
     return int(input("What do you want to do?\n[1] Show your expenses\n[2] Modify expenses\n[3] Exit:\n"))
 
 
 def read_data():
+    """Read the data from file and return a list of them"""
     with open("L6_list_tuple\\14_expense.txt", "r") as f:
         list = f.readlines()
         for i in range(len(list)):
@@ -40,12 +46,28 @@ def read_data():
 
 
 def modify_data():
-    with open("L6_list_tuple\\14_expense.txt", "r") as f:
-        pass
+    """Read the data into a list, modify it and write the file with the updated list"""
+    old_data = read_data()
+    categories = ['Rent', 'Gas', 'Food', 'Clothing', 'Car Payment', 'Misc']
+    while True:
+        choice = int(input("Which one do you want to modify?\n[1] Rent\n[2] Gas\n[3] Food\n[4] Clothing\n[5] Car Payment\n[6] Misc:\n"))
+        if choice >= 1 and choice <= 6:
+            new = int(input(f"Enter the new value for '{categories[choice - 1]}': "))
+            old_data[choice - 1] = new
+            break
+        else:
+            print("Please insert a valid choice.")
+        choice = int(input("Which one do you want to modify?\n[1] Rent\n[2] Gas\n[3] Food\n[4]Clothing\n[5] Car Payment\n[6]Misc:\n"))
+        
+    with open("L6_list_tuple\\14_expense.txt", "w") as f:
+        for _, num in enumerate(old_data):
+            f.write(str(num) + '\n')
+        print("Your expenses has been updated.\n")
 
+                
 
 def show_graph(expenses):
-    # Create a list of labels for the slices. 
+    """Show the expenses graph"""
     slice_labels = ['Rent', 'Gas', 'Food', 'Clothing', 'Car Payment', 'Misc']
     
     greatest = max(expenses)
@@ -56,7 +78,6 @@ def show_graph(expenses):
     fig1, ax1 = plt.subplots()
     ax1.pie(expenses, explode=explode, labels=slice_labels, autopct='%1.1f%%',
             shadow=True, startangle=90)
-    # Equal aspect ratio ensures that pie is drawn as a circle
     centre_circle = plt.Circle((0,0),0.75,fc='white')
     fig = plt.gcf()
     fig.gca().add_artist(centre_circle)
